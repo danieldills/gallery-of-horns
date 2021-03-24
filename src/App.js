0634978814e99cd273bd99bca65e34d69e7463ba
Daniel Dills <dills253@gmail.com>
2021-03-23 21:45:04 -0700

    added time est to README, updated naming convention, updated Header.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import Header from './Header.js';
-import Main from './Main.js';
-import Footer from './Footer.js';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
 import HornedBeast from './HornedBeast';
 
 class App extends React.Component {
--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Header extends React.Component {
     render() {
-        return(<h1>Gallery of Horned Beasts</h1>);
+        return(<header><h1>Gallery of Horned Beasts</h1></header>);
     }
 }
 
