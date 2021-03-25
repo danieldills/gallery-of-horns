a9e389fd734f5719106e774b0479f11c6ba58eee
Daniel Dills <dills253@gmail.com>
2021-03-24 17:21:48 -0700

    completed Feature 1 & 2, updated README file

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ import './App.css';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
-import HornedBeast from './HornedBeast';
+// import HornedBeast from './HornedBeast';
+import Data from './data.json';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      beastData: Data,
+    }
+  }
+  
   render() {
     return (
       <div>
         <Header /> 
-        <Main />
+        <Main beasts={this.state.beastData} />
         <Footer />
       </div>
     )
--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+
 
 class HornedBeast extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            favoriteBeasts: 0
+        }
+    }
+
+    numOfClicks = () => {
+        this.setState({ favoriteBeasts: this.state.favoriteBeasts + 1 });
+    }
+
     render() {
-        return(
+        return (
             <div>
-                <h2>{this.props.title}</h2>
-                <img src={this.props.img_url} alt={this.props.alt} title={this.props.title} />
-                <p>{this.props.description}</p>
+                <Card style={{ width: '15rem' }} onClick={this.numOfClicks}>
+                    <Card.Img variant="top" src={this.props.pic} />
+                    <Card.Body>
+                        <Card.Title>{this.props.title}</Card.Title>
+                        <Card.Text>
+                            {this.props.description}
+                        </Card.Text>
+                        <Card.Text>
+                            💜 = {this.state.favoriteBeasts}
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
             </div>
         )
     }
